6298f252208751a39a7148ea34b064d840490860
B-Lowe <brenden.lowe@me.com>
2020-02-26 19:46:29 -0800

    Implemented first_and_last function and corresponding unit tests

--- task.py
+++ task.py
@@ -8,3 +8,7 @@ def firstrun():
 def circle_area(radius):
     area = radius * (math.pi ** 2)
     return area
+
+
+def first_and_last(some_list):
+    return "first: " + str(some_list[0]) + ", last: " + str(some_list[len(some_list)-1])
--- tests.py
+++ tests.py
@@ -2,6 +2,7 @@ import unittest
 import task
 import math
 import random
+import time
 
 
 class TestCase(unittest.TestCase):
@@ -20,6 +21,20 @@ class TestCase(unittest.TestCase):
         for i in range(0, 5):
             random_int = random.randint(0, 100)
             self.assertEqual(task.circle_area(random_int), random_int * (math.pi ** 2))
+
+    def test_first_and_last(self):
+        # Test list of integers
+        # Define random list size
+        random.seed(time.time())
+        list_size = random.randint(0, 100)
+        # Define a list of ints
+        list_int = []
+        # Add random ints to list
+        for i in range(list_size):
+            list_int.append(random.randint(0, 100))
+        # Assert that the function returns first and last elements of list
+        expected = "first: " + list_int[0] + ", last: " + list_int[list_size - 1]
+        self.assertEqual(task.first_and_last(list_int), expected)
 
 
 if __name__ == "__main__":
