38ed59b1f18530693259a0aa4c304df01f0a44e7
B-Lowe <brenden.lowe@me.com>
2020-03-01 21:32:27 -0800

    Added function diff_days and corresponding unit test

--- task.py
+++ task.py
@@ -1,4 +1,5 @@
 import math
+import datetime
 
 
 def firstrun():
@@ -12,3 +13,8 @@ def circle_area(radius):
 
 def first_and_last(some_list):
     return "first: " + str(some_list[0]) + ", last: " + str(some_list[len(some_list)-1])
+
+
+def diff_days(first_date, second_date):
+    diff_days = second_date - first_date
+    return diff_days.days
--- tests.py
+++ tests.py
@@ -3,6 +3,7 @@ import task
 import math
 import random
 import time
+import datetime
 
 
 class TestCase(unittest.TestCase):
@@ -35,6 +36,21 @@ class TestCase(unittest.TestCase):
         # Assert that the function returns first and last elements of list
         expected = "first: " + str(list_int[0]) + ", last: " + str(list_int[list_size - 1])
         self.assertEqual(task.first_and_last(list_int), expected)
+
+    def test_diff_days(self):
+        # Test datetime object, days in same month
+        first_date = datetime.datetime(2020, 3, 20)
+        second_date = datetime.datetime(2020, 3, 30)
+        # Assert that diff_days returns 10
+        self.assertEqual(task.diff_days(first_date, second_date), 10)
+        # Test datetime object, days in different month
+        first_date = datetime.datetime(2020, 1, 1)
+        second_date = datetime.datetime(2020, 2, 1)
+        self.assertEqual(task.diff_days(first_date, second_date), 31)
+        # Test datetime object, days in different years
+        first_date = datetime.datetime(2019, 1, 1)
+        second_date = datetime.datetime(2020, 1, 1)
+        self.assertEqual(task.diff_days(first_date, second_date), 365)
 
 
 if __name__ == "__main__":
